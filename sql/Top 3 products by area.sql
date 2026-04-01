3dbf106c1e7edad6e8443e9351ad7d8b31025974
itmitev <itmitev@gmail.com>
2026-04-01 13:41:32 +0200

    Add files via upload

---
+++ sql/Top 3 products by area.sql
@@ -0,0 +1,34 @@
+with
+  area_prod as (
+    select
+      c.area,
+      p.product_name,
+      sum(
+        coalesce(oi.quantity, 0) * coalesce(oi.unit_price, 0)
+      ) as revenue
+    from
+      customers c
+      join orders o on o.customer_id = c.customer_id
+      left join order_items oi on oi.order_id = o.order_id
+      left join products p on p.product_id = oi.product_id
+    group by
+      1,
+      2
+  ),
+  area_prod_ranked as (
+    select
+      ap.*,
+      RANK() over (
+        partition by
+          area
+        order by
+          revenue desc
+      ) revenue_rank
+    from
+      area_prod ap
+  )
+select
+  *
+from
+  area_prod_ranked
+  where revenue_rank <=3
